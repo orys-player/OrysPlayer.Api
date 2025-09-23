b0e1364751838eabeec7a74134afe13625c4cbc4
horameus <ludo.hartmann@gmail.com>
2025-09-23 16:45:04 +0200

    creating a test User controller

---
+++ OrysPlayer.Api/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrysPlayerApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private static readonly List<User> Users =
+    [
+        new User { Id = 1, Firstname = "John", Lastname = "Doe", Email = "john.doe@gmail.com", Password = "password" },
+        new User { Id = 2, Firstname = "Jane", Lastname = "Doe", Email = "jane.doe@gmail.com", Password = "password" },
+        new User { Id = 3, Firstname = "Jim", Lastname = "Beam", Email = "jim.beam@gmail.com", Password = "password" }
+    ];
+
+    [HttpGet]
+    public ActionResult<List<User>> GetUsers()
+    {
+        return Ok(Users);
+    }
+}
---
+++ OrysPlayer.Api/User.cs
@@ -0,0 +1,10 @@
+﻿namespace OrysPlayerApi;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Firstname { get; set; }
+    public string? Lastname { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}
