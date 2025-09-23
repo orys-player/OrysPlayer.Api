ea4cd4f014ad501038b0c4ec2493224ec64338c2
horameus <ludo.hartmann@gmail.com>
2025-09-23 15:07:13 +0200

    test3

--- OrysPlayer.Api/Program.cs
+++ OrysPlayer.Api/Program.cs
@@ -3,7 +3,6 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
