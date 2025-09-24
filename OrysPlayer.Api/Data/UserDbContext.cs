81772c13e048a6036aa1d6ac987521605fbcfd10
horameus <ludo.hartmann@gmail.com>
2025-09-24 16:53:53 +0200

    Created the DbContext

---
+++ OrysPlayer.Api/Data/UserDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrysPlayerApi.Data;
+
+public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+}
--- OrysPlayer.Api/Program.cs
+++ OrysPlayer.Api/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using OrysPlayerApi.Data;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<UserDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
